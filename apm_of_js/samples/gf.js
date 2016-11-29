4259b303fa43d2d3a7e6e66572aa0a226218bc77
fsjohnhuang <fsjohnhuang@hotmail.com>
2016-11-29 09:50:52 +0800

    update

--- apm_of_js/samples/gf.js
+++ apm_of_js/samples/gf.js
@@ -13,21 +13,19 @@ const c = v蛋液 => new Promise(doAsyncIO('半熟的鸡蛋'))
 const d = v半熟的鸡蛋 => new Promise(doAsyncIO('鸡蛋块'))
 const e = (v番茄块, v鸡蛋块) => new Promise(doAsyncIO('番茄炒鸡蛋'))
 
-/* 执行任务 */
-exports.exec = function(err = false){
-	let pErr = new Promise((resolve, reject)=>setTimeout(()=>reject(Error('鸡蛋坏了!')), Math.random() * 1000))
 
-	iPromise(function *(){
-		try{
-			var p番茄块 = a('番茄')
-			var v蛋液 = yield (err ? pErr : (pErr.catch(e=>{}), b('鸡蛋')))
-			var v半熟的鸡蛋 = yield c(v蛋液)
-			var v鸡蛋块 = yield d(v半熟的鸡蛋)
-			var v番茄块 = yield p番茄块
-			var v番茄抄鸡蛋 = yield e(v番茄块, v鸡蛋块)
-		}
-		catch(e){
-			console.log(e.message)
-		}
-	})
-}
+/* 执行任务 */
+exports.exec = iPromise(function* (err=false){
+	let pErr = new Promise((resolve, reject)=>setTimeout(()=>reject(Error('鸡蛋坏了!')), Math.random() * 1000))
+	try{
+		var p番茄块 = a('番茄')
+		var v蛋液 = yield (err ? pErr : (pErr.catch(e=>{}), b('鸡蛋')))
+		var v半熟的鸡蛋 = yield c(v蛋液)
+		var v鸡蛋块 = yield d(v半熟的鸡蛋)
+		var v番茄块 = yield p番茄块
+		var v番茄抄鸡蛋 = yield e(v番茄块, v鸡蛋块)
+	}
+	catch(e){
+		console.log(e.message)
+	}
+})
